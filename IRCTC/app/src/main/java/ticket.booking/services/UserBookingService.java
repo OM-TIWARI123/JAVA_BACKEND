71506121273e9d245889be972cf9df399322a40d
OM_TIWARI123 <90179771+OM-TIWARI123@users.noreply.github.com>
2025-08-18 18:14:53 +0530

    init

---
+++ IRCTC/app/src/main/java/ticket.booking/services/UserBookingService.java
@@ -0,0 +1,20 @@
+package ticket.booking.services;
+
+import java.io.File;
+
+import ticket.booking.entities.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class UserBookingService {
+
+    private User user;
+
+    private static final OBJECT_MAPPER = new ObjectMapper(); 
+    private static final String USERS_PATH = "../localDb/users.json";
+
+    
+    public UserBookingService(User user1){
+        this.user = user1;
+        File users = new File(USERS_PATH);
+    }
+}
---
+++ test.java
@@ -0,0 +1,13 @@
+public class test{
+
+    public static void main(String[]args){
+        System.out.println("Hello world");
+    }
+     public static void main2(String[]args){
+        System.out.println("Hello world 2");
+    }
+     public static void main3(String[]args){
+        System.out.println("Hello world 3");
+    }
+
+}
